b997babe4f55346aabfa9c8d5b57709836d33cf6
Kane Clover <kane@assure24.com>
2018-09-04 14:06:03 +0100

    Firebase auth popups test

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  firebase: {
+    apiKey: "AIzaSyAcRXDgG5ILo4L8NoF_ZtSaV9_e6jNacw0",
+    authDomain: "foldapp-8df86.firebaseapp.com",
+    databaseURL: "https://foldapp-8df86.firebaseio.com",
+    projectId: "foldapp-8df86",
+    storageBucket: "",
+    messagingSenderId: "825457505870"
+  }
+}
+
+export default config
