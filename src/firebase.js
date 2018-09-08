68df4bbdcdb0f46148be26bd3f105e6b9b59bab1
Kane Clover <kane@assure24.com>
2018-09-08 21:13:26 +0100

    enable offline

--- src/firebase.js
+++ src/firebase.js
@@ -3,4 +3,19 @@ import config from './config'
 
 firebase.initializeApp(config.firebase)
 
+firebase.firestore().enablePersistence()
+  .catch(function(err) {
+    console.log(err)
+    if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code == 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+    // TODO: Show warnings
+  });
+
 export const db = firebase.firestore()
