8aff267f76237bfa9a059d0ec181839eec56a8c0
Kane Clover <kane@assure24.com>
2018-09-10 16:09:50 +0100

    Hot reloading

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root')
+
+ReactDOM.render(
+  <App />,
+  rootEl
+)
+
+if (module.hot) {
+  // See https://medium.com/superhighfives/hot-reloading-create-react-app-73297a00dcad
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    )
+  })
+}
+
 registerServiceWorker();
