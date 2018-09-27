74e7cfb0f98b49c3a677ba116382b4c0d9b8fa7b
Kane Clover <kane@assure24.com>
2018-09-27 12:04:21 +0100

    Show URL + better status toggle

--- src/components/Fold.js
+++ src/components/Fold.js
@@ -7,6 +7,8 @@ import { colorFromString } from '../helpers/color'
 import SmallTag from './SmallTag'
 import SmallButton from './SmallButton'
 import media from '../helpers/mediaQueries'
+import URL from 'url-parse'
+import tinycolor from 'tinycolor2'
 
 class Fold extends React.Component {
 
@@ -20,22 +22,27 @@ class Fold extends React.Component {
 
     const { fold: { title, uri, tags, description, createdAt }, edit, remove, setFilter } = this.props
     const color = colorFromString(tags[0])
+    const btnColor = tinycolor(color).spin(180).toString()
 
     const dateDisplay = distanceInWordsToNow(createdAt.toDate(), { addSuffix: true });
+    const urlParsed = new URL(uri)
+    const { hostname, pathname } = urlParsed
 
     return (
       <Outer>
         <Card style={{ background: color }}>
-          <Content>
-            <Title>
-              <h4><a href={uri} target="new">{title}<Icon>link</Icon></a></h4>
-              <IconButton onClick={this.toggleExpanded} icon="more_vert"/>
-            </Title>
-            <Date>{dateDisplay}</Date>
-            <Description>
-              {description}
-            </Description>
-          </Content>
+          <Title>
+            <h4><a href={uri} target="new">{title}</a></h4>
+          </Title>
+          <Path><Icon>link</Icon>{hostname}{pathname && <span>{pathname}</span>}</Path>
+          <Date><Icon>access_time</Icon>{dateDisplay}</Date>
+          <Description>
+            {description}
+          </Description>
+          <StatusToggle style={{ background: btnColor }} onClick={this.toggleExpanded}>
+            <Icon>more_vert</Icon>
+            <Icon>mode_comment</Icon>
+          </StatusToggle>
           <Tags>
             {tags.map(tag => <SmallTag key={tag} text={tag} onClick={() => setFilter([tag])}/>)}
           </Tags>
@@ -59,10 +66,12 @@ const Outer = styled.div`
 `
 const Card = styled.div`
   margin: .5rem 0 0 .5rem;
+  position: relative;
 `
 
 const Title = styled.div`
   display: flex;
+  padding: .5rem .5rem 0;
   h4 {
     flex: 1;
     white-space: nowrap;
@@ -73,38 +82,70 @@ const Title = styled.div`
     text-decoration: none;
     color: inherit;
   }
+`
+
+const Path = styled.div`
+  font-size: .707rem;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  padding: 0 .5rem .5rem;
+  span {
+    opacity: .6;
+  }
   .material-icons {
-    margin-left: .5rem;
+    font-size: 1rem;
+    margin-right: .5rem;
     vertical-align: middle;
   }
 `
 
-const Content = styled.div`
-  padding: .5rem;
+const Date = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: .707rem;
+  padding: 0 .5rem;
+  .material-icons {
+    font-size: 1rem;
+    margin-right: .5rem;
+  }
 `
 
+// TODO: Ellipsis doen't work here... Not an elegant CSS solution really.
+// Maybe http://hackingui.com/front-end/a-pure-css-solution-for-multiline-text-truncation/
 const Description = styled.div`
-  padding: 1rem 0;
+  padding: 1rem .5rem;
   text-overflow: ellipsis;
   height: 4rem;
   overflow: hidden;
 `
 
-const Date = styled.div`
-  font-size: .707rem;
-`
-
 const Tags = styled.div`
-  background: rgba(255,255,255,0.2);
   padding: .5rem;
   height: 1.3rem;
   white-space: nowrap;
   overflow: hidden;
 `
 
+const StatusToggle = styled.div`
+  position: absolute;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  bottom: .5rem;
+  right: .5rem;
+  width: 3rem;
+  padding: .4rem;
+  border-radius: 1rem;
+  .material-icons {
+    font-size: 1rem;
+    color: white;
+  }
+`
+
 const Actions = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
   background-image: 
   linear-gradient(
     to right,
